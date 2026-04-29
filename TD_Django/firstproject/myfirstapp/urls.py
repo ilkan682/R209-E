ebb20359f88224109a8f223ef8b7cdc706318eb9
e2500435 <g682ilkan@gmail.com>
2026-04-29 09:44:41 +0200

    -

--- TD_Django/firstproject/myfirstapp/urls.py
+++ TD_Django/firstproject/myfirstapp/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 urlpatterns = [
     path("index/", views.index),
-    path("formulaire/",views.index)
+    path("formulaire/",views.formulaire),
+    path("bonjour/",views.bonjour),
+
 ]
--- TD_Django/firstproject/myfirstapp/views.py
+++ TD_Django/firstproject/myfirstapp/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 #create your views here.
 def index(request):
     return render(request, 'myfirstapp/index.html')
-def formulaire(requests):
-    return render(requests, "myfirstapp/formulaire.html")
+def formulaire(request):
+    return render(request, "myfirstapp/formulaire.html")
+def bonjour (request):
+    nom = request.GET["nom"]
+    prenom = request.GET["prenom"]
+    return  render(request, "myfirstapp/bonjour.html", {"nom":nom, "prenom":prenom})
+
