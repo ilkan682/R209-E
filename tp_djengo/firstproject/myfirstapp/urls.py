655fd1a5dbcc6951df17df0c1812517353b6bae2
e2500435 <g682ilkan@gmail.com>
2026-05-07 16:55:10 +0200

    ddd

--- tp_djengo/firstproject/myfirstapp/urls.py
+++ tp_djengo/firstproject/myfirstapp/urls.py
@@ -2,6 +2,9 @@ from . import views
 from django.urls import path
 urlpatterns = [
     path("ajout/", views.ajout),
+    path('traitement', views.traitement),
+    path('/affiche/<int:id>/',views.read),
+    path('/update/<int:id>/',views.update),
 
 
 
--- tp_djengo/firstproject/myfirstapp/views.py
+++ tp_djengo/firstproject/myfirstapp/views.py
@@ -14,3 +14,29 @@ def ajout(request):
     else :
         form = LivreForm()
         return render(request,"myfirstapp/ajout.html",{"form" : form})
+
+def traitement(request):
+    lform = LivreForm(request.POST)
+    if lform.is_valid():
+        Livre = lform.save()
+        return render(request,"myfirstapp/affiche.html",{"Livre" : Livre})
+    else:
+        return render(request,"myfirstapp/ajout.html",{"form": lform})
+
+def read(request, id):
+        Livre = models.Livre.objects.get(pk=id)
+
+        return render(request,"myfirstapp/affiche.html",{"Livre": Livre})
+
+def traitementupdate(request, id):
+    lform = LivreForm(request.POST)
+    if lform.is_valid():
+        Livre = lform.save(commit=False)
+        Livre.id = id;
+        Livre.save()
+        return HttpResponseRedirect("/myfirstapp/")
+
+    else:
+        return render(request, "myfirstapp/update.html", {"form": lform, "id": id})
+
+
