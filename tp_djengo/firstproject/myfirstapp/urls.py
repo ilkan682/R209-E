a2715fe4cbaddc751cab1cd156eb0ce8d6000efb
e2500435 <g682ilkan@gmail.com>
2026-05-07 16:58:54 +0200

    ddd

--- tp_djengo/firstproject/myfirstapp/urls.py
+++ tp_djengo/firstproject/myfirstapp/urls.py
@@ -4,7 +4,7 @@ urlpatterns = [
     path("ajout/", views.ajout),
     path('traitement', views.traitement),
     path('/affiche/<int:id>/',views.read),
-    path('/update/<int:id>/',views.update),
+    path('/update/<int:id>/',views.traitementupdate),
 
 
 
