440dcabe436414303bff73b470cf3c22ddf318f3
e2500435 <g682ilkan@gmail.com>
2026-05-18 14:14:50 +0200

    e

--- tp_djengo/firstproject/myfirstapp/urls.py
+++ tp_djengo/firstproject/myfirstapp/urls.py
@@ -1,10 +1,11 @@
 from . import views
 from django.urls import path
+
 urlpatterns = [
-    path('', views.index),
-    path("ajout/", views.ajout),
-    path('traitement', views.traitement),
-    path('affiche/<int:id>/',views.read),
-    path('update/<int:id>/',views.traitementupdate),
-    path('delete/<int:id>/', views.delete),
+    path('', views.index, name='index'),
+    path("ajout/", views.ajout, name='ajout'),
+    path('traitement/', views.traitement, name='traitement'), # Ajout du name
+    path('affiche/<int:id>/', views.read, name='affiche'),
+    path('update/<int:id>/', views.traitementupdate, name='update'),
+    path('delete/<int:id>/', views.delete, name='delete'),
 ]
--- tp_djengo/firstproject/myfirstapp/views.py
+++ tp_djengo/firstproject/myfirstapp/views.py
@@ -1,53 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect  # Importations ajoutées ici
 from django.http import HttpResponseRedirect
 from .forms import LivreForm
 from . import models
 
+# Crée un nouveau livre
 def ajout(request):
     if request.method == "POST":
         form = LivreForm(request.POST)
         if form.is_valid():
-             Livre = form.save()
-             return render(request,"myfirstapp/affiche.html",{"Livre" : Livre})
-
+            Livre = form.save()
+            return render(request, "myfirstapp/affiche.html", {"Livre": Livre})
         else:
-            return render(request,"myfirstapp/ajout.html",{"form": form})
-    else :
+            return render(request, "myfirstapp/ajout.html", {"form": form})
+    else:
         form = LivreForm()
-        return render(request,"myfirstapp/ajout.html",{"form" : form})
+        return render(request, "myfirstapp/ajout.html", {"form": form})
 
+# Ancienne fonction de traitement (gardée si utilisée ailleurs, sinon tu peux la supprimer)
 def traitement(request):
     lform = LivreForm(request.POST)
     if lform.is_valid():
         Livre = lform.save()
-        return render(request,"myfirstapp/affiche.html",{"Livre" : Livre})
+        return render(request, "myfirstapp/affiche.html", {"Livre": Livre})
     else:
-        return render(request,"myfirstapp/ajout.html",{"form": lform})
+        return render(request, "myfirstapp/ajout.html", {"form": lform})
 
+# Affiche un livre spécifique
 def read(request, id):
-        Livre = models.Livre.objects.get(pk=id)
+    Livre = models.Livre.objects.get(pk=id)
+    return render(request, "myfirstapp/affiche.html", {"Livre": Livre})
 
-        return render(request,"myfirstapp/affiche.html",{"Livre": Livre})
-
-def update(request, id):
-    livre = models.Livre.objects.get(pk=id)
-
-    form = LivreForm(instance=livre)
-    return render(request, "myfirstapp/update.html", {"form": form, "id": id})
-
+# Gère l'affichage ET la modification d'un livre (GET et POST)
 def traitementupdate(request, id):
-    lform = LivreForm(request.POST)
-    if lform.is_valid():
-        Livre = lform.save(commit=False)
-        Livre.id = id;
-        Livre.save()
-        return HttpResponseRedirect("/myfirstapp/")
+    # On récupère le livre ou on renvoie une vraie erreur 404 proprement si l'ID n'existe pas
+    livre = get_object_or_404(models.Livre, pk=id)
 
+    if request.method == "POST":
+        # On lie le formulaire aux données envoyées (POST) et au livre existant (instance)
+        lform = LivreForm(request.POST, instance=livre)
+        if lform.is_valid():
+            lform.save()  # Met à jour le livre existant
+            return redirect('index')  # Redirige vers la page d'accueil
+        else:
+            return render(request, "myfirstapp/update.html", {"form": lform, "id": id})
     else:
+        # Requête GET : On pré-remplit le formulaire avec les données du livre
+        lform = LivreForm(instance=livre)
         return render(request, "myfirstapp/update.html", {"form": lform, "id": id})
 
-
-# Affiche la liste de tous les livres (Vue "All")
+# Affiche la liste de tous les livres
 def index(request):
     tous_les_livres = list(models.Livre.objects.all())
     return render(request, "myfirstapp/index.html", {"liste": tous_les_livres})
@@ -57,5 +58,3 @@ def delete(request, id):
     livre = models.Livre.objects.get(pk=id)
     livre.delete()
     return HttpResponseRedirect("/myfirstapp/")
-
-
